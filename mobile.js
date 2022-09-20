2375e88597bbd090ba381f38433abd433ea76adb
Ogaga <iyaraogaga69@gmail.com>
2022-09-20 18:09:35 +0100

    Redo desktop pop-up and add click events

--- mobile.js
+++ mobile.js
@@ -2,6 +2,10 @@ const openMenu = document.getElementById("menu-bar");
 const closeMenu = document.querySelector(".close");
 const popUp = document.getElementById("menu-list");
 const menuList = document.querySelectorAll(".menu-li");
+const btnProject = document.querySelectorAll('.project-button')
+const project = document.querySelector('.pop-up')
+const closeProject = document.querySelector('.project-close')
+console.log(btnProject)
 
 openMenu.addEventListener("click", () => {
   popUp.classList.add("show");
@@ -14,3 +18,17 @@ menuList.forEach((item) => {
     popUp.classList.remove("show");
   });
 });
+
+const openPopUp = function () {
+  project.classList.remove('hidden')
+}
+
+const closePopUp = function () {
+  project.classList.add('hidden')
+}
+
+closeProject.addEventListener('click', closePopUp)
+
+for (let i = 0; i < btnProject.length; i++){
+  btnProject[i].addEventListener('click', openPopUp)
+}
