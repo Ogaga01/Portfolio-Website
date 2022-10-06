df91ef70d46c2b905cb9b04f37a9ab32a77d9b0d
Ogaga <iyaraogaga69@gmail.com>
2022-10-06 11:35:48 +0100

    Add smooth scrolling to nav links

--- mobile.js
+++ mobile.js
@@ -5,6 +5,8 @@ const menuList = document.querySelectorAll(".menu-li");
 const btnProject = document.querySelectorAll('.project-button')
 const project = document.querySelector('.pop-up')
 const closeProject = document.querySelector('.project-close')
+const navBar = document.getElementById('navbar-list')
+const navBarList = document.querySelectorAll('.navbar-li')
 console.log(btnProject)
 
 openMenu.addEventListener("click", () => {
@@ -32,3 +34,12 @@ closeProject.addEventListener('click', closePopUp)
 for (let i = 0; i < btnProject.length; i++){
   btnProject[i].addEventListener('click', openPopUp)
 }
+
+navBar.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(e.target)
+  if (e.target.classList.contains('list-link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
